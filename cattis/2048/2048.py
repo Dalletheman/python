c75b8d5f89f7f2806ad0e048d0a92665aaa426ce
Dalenius Dennis <dennis.dalenius@afconsult.com>
2019-10-31 16:27:35 +0100

    changed 2048

--- cattis/2048/2048.py
+++ cattis/2048/2048.py
@@ -1,6 +1,8 @@
 import sys
 input = False
 
+# 0 = left; 1 = up; 2 = right; 3 = down
+
 def readdata(input):
     if input:
         data = sys.stdin.read().splitlines()
@@ -9,15 +11,42 @@ def readdata(input):
             data = f.read().splitlines()
     return data
 
+def list_2_matrix(str_list):
+    matrix = []
+    for i in range(0, length - 1):
+        temp = str_list[i].split(' ')
+        temp = list(map(int, temp))
+        matrix.append(temp)
+    return matrix
+
 data = readdata(input)
 length = len(data)
+dir = int(data[length - 1])
+matrix = list_2_matrix(data)
+print(matrix)
+for i in range(0,4):
+    for j in range(1, 4):
+        status = True
+        action_index = -1
+        index = j - 1
+        if matrix[i][j] != 0:
 
+            while status:
 
-matrix = []
+                if matrix[i][index + 1] == matrix[i][index]:
+                    matrix[i][index] = matrix[i][index]*2
+                    matrix[i][index + 1] = 0
+                    action_index = index
+                    status = False
+                elif matrix[i][index] == 0:
+                    matrix[i][index] = matrix[i][index + 1]
+                    matrix[i][index + 1] = 0
+                else:
+                    status = False
 
-for i in range(0,length - 1):
-    temp = data[i].split(' ')
-    temp = list(map(int,temp))
-    matrix.append(temp)
+                index -= 1
+                if index == action_index:
+                    status = False
+
 
 print(matrix)
